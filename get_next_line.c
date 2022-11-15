e9e9538c615109e5bb91e3092a816be9941fc0b9
Nick van den Dungen <nicktvdd@hotmail.com>
2022-11-15 10:48:03 +0200

    i got the first line

--- get_next_line.c
+++ get_next_line.c
@@ -6,18 +6,33 @@
 /*   By: nvan-den <nvan-den@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 12:36:32 by nvan-den          #+#    #+#             */
-/*   Updated: 2022/11/15 10:29:02 by nvan-den         ###   ########.fr       */
+/*   Updated: 2022/11/15 10:47:06 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdlib.h>
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && s[i] != '\n')
+	{
+		i++;
+	}
+	return (i);
+}
 
 char	*get_next_line(int fd)
 {
 	static int	ret;
-	static char	buf[BUFFER_SIZE + 1];
+	char	*buf;
 	static int	once;
+	size_t	len;
 
+	buf = (char*)malloc(BUFFER_SIZE + 1);
 	once = 0;
 
 	ret = 0;
@@ -28,11 +43,10 @@ char	*get_next_line(int fd)
 		ret = read(fd, buf, BUFFER_SIZE);
 		once++;
 	}
-/* 	while (ret && ret != '\n')
-	{ */
-		buf[ret] = '\0';
-/* 		ret = read(fd, buf, BUFFER_SIZE); */
-/* 	} */
+	buf[ret] = '\0';
+	len = ft_strlen(buf);
+	buf[len] = '\0';
+
 return (buf);
 
 /* 	ft_linelenght(ret) */
--- test.c
+++ test.c
@@ -16,7 +16,7 @@ int	main(int argc, char **argv)
 	fd = open(argv[1], O_RDONLY);
 	if (fd)
 	{
-		while (i < 3)
+		while (i < 1)
 		{
 			printf("%s", get_next_line(fd));
 			i++;
